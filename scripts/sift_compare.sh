3b4f00a72a23573d3b47be564387113fa030cb67
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2021-05-07 16:37:58 +0800

    save change

--- scripts/sift_compare.sh
+++ scripts/sift_compare.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+name=$1
 
-cmd="python sift_compare.py --input_gr results/kovan/chinatown10_dag2/test_latest/images/real_A/ \
---input_gen results/kovan/chinatown10_dag2/test_latest/images/real_B --output_file results/kovan/realA_realB.csv"
-
-echo $cmd
-eval $cmd
-
-cmd="python sift_compare.py --input_gr results/kovan/chinatown10_dag2/test_latest/images/fake_B/ \
---input_gen results/kovan/chinatown10_dag2/test_latest/images/real_B --output_file results/kovan/fakeB_realB.csv"
+cmd="python sift_compare.py --input_gr results/kovan/${name}/test_latest/images/real_A/ \
+--input_gen results/kovan/${name}/test_latest/images/real_B --output_file results/kovan/realA_realB.csv"
 
 echo $cmd
 eval $cmd
 
-cmd="python sift_compare.py --input_gr results/kovan/chinatown10_dag2/test_latest/images/fake_B/ \
---input_gen results/kovan/chinatown10_dag2/test_latest/images/real_A --output_file results/kovan/fakeB_realA.csv"
+cmd="python sift_compare.py --input_gr results/kovan/${name}/test_latest/images/fake_B/ \
+--input_gen results/kovan/${name}/test_latest/images/real_B --output_file results/kovan/fakeB_realB.csv"
+
+echo $cmd
+eval $cmd
+
+cmd="python sift_compare.py --input_gr results/kovan/${name}/test_latest/images/fake_B/ \
+--input_gen results/kovan/${name}/test_latest/images/real_A --output_file results/kovan/fakeB_realA.csv"
 
 echo $cmd
 eval $cmd
